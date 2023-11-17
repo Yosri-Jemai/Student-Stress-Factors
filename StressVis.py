af684d2ebe4de724c2ca3cce4e09c40b4ad04a44
unknown <yosrii.jemaiii@gmail.com>
2023-11-18 00:42:17 +0100

    V2

--- StressVis.py
+++ StressVis.py
@@ -1,22 +1,24 @@
-import numpy as np
+# Importing required libraries
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import warnings
 
-#Reading and loading CSV
-df = pd.read_csv('DataSet/StressLevelDataset.csv')
-print(df.head(), "\n")
+# Loading dataset to Notebook via pandas 
+df = pd.read_csv("DataSet/StressLevelDataset.csv")
+df.head()
 
-#All columns
-print("We have ", len(df.columns), " Columns")
-print(df.columns, "\n")
-
-#getting basic information about df like  How many students are in the dataset?
+# Basic information about df like  How many students are in the dataset
 df.info()
 
-#he number of unique values for each column
-print(df.nunique())
+# Statitical information for numeric columns 
+df.describe().round(2)
+
+# Checking for duplicates 
+df.duplicated().sum()
+
+# Checking if we have missing values 
+df.isna().sum()
 
 #Data Display
 r = 3
@@ -31,4 +33,13 @@ for i in df.columns:
         sns.countplot(x=df[i])
     it += 1
 plt.tight_layout()
+plt.show()
+
+# Calculate the correlation of every column
+correlation_df= df.corr()
+
+# Create a heatmap
+plt.figure(figsize=(14, 10))
+sns.heatmap(data=correlation_df, annot=True, cmap='coolwarm')
+plt.title("What tends to a higher stress level?", fontweight='bold')
 plt.show()
